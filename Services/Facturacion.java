c38455a33f71b47d3adc8139dcfa1dca01b7ac94
Emiliano Estevarena <65693639+emiestevarena@users.noreply.github.com>
2020-09-24 19:01:54 -0300

    Add files via upload

--- Services/Facturacion.java
+++ Services/Facturacion.java
@@ -37,6 +37,13 @@ public class Facturacion {
         this.numeroFactura(cc, c);
         this.numeroPoliza(c,p);
         this.numeroDeCuota(c);
+        this.monto(c);
+        this.pagada(c);
+        this.vencimiento(c);
+        this.formaDePago(c,p);
+        cc.getCuotas().add(c);
+        c=null;
+        p=null;
     }
 
     private void numeroFactura(Cuotas cc,Cuota c){
@@ -71,5 +78,89 @@ public class Facturacion {
     private int numeroCuotaOK(int a) throws MyException{
         if(a<1) throw new MyException("Número negativo");
         else return a;
+    }
+
+    private void monto(Cuota c){
+        double a=inputMonto();
+        while(a==0){a=inputMonto();}
+        c.setMonto(a);
+    }
+
+    private double inputMonto(){
+        double a=0;
+        System.out.println("Ingrese monto:");
+        try{
+            a=montoOK(leer.nextDouble());
+        }catch(Exception e){
+            System.out.println("Hubo un error");
+        }
+        return a;
+    }
+
+    private double montoOK(double a) throws MyException{
+        if(a<=0) throw new MyException("Número negativo");
+        else return a;        
+    }
+
+    private void pagada(Cuota c){
+        int a=inputPagada();
+        if(a==1) c.setPagada(true);
+        else c.setPagada(false);
+    }
+
+    private int inputPagada(){
+        int a=0;
+        System.out.println("Ingrese 1 para indicar si la cuota fue pagada");
+        try{
+            a=leer.nextInt();
+        }catch(Exception e){
+            System.out.println("Hubo un error");
+        }
+        return a;
+    }
+
+    private void vencimiento(Cuota c){
+        int y=inputYear();
+        while(y==0){y=inputYear();}
+        int m=inputMonth();
+        while(m==0){m=inputMonth();}
+        GregorianCalendar g = new GregorianCalendar(y,m,10);
+        c.setVencimiento(g);
+    }    
+
+    private int inputYear(){
+        int a=0;
+        try{
+            System.out.println("Ingrese año");
+            a=añoOK(leer.nextInt());
+        }catch(Exception e){
+            System.out.println("Ha ocurrido un error");
+        }
+        return a;
+    }
+
+    private int añoOK(int a) throws MyException{
+        if(a<1900) throw new MyException("Año anterior a 1900");
+        else return a;
+    }
+
+    private int inputMonth(){
+        int a=0;
+        try{
+            System.out.println("Ingrese numero");
+            a=mesOK(leer.nextInt());
+        }catch(Exception e){
+            System.out.println("Ha ocurrido un error");
+        }
+        return a;
+    }
+
+    private int mesOK(int a) throws MyException{
+        if(a>12 || a<1) throw new MyException("Mes fuera de rango");
+        else return a;
+    }
+
+    private void formaDePago(Cuota c,Poliza p){
+        c.setFormaDePago(p.getFormaDePago());
     }
 }
