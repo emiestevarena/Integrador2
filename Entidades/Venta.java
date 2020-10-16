e63491acf583ceeb87895d3ecca40c53bed90adc
IvanBerenguer <36020676+IvanBerenguer@users.noreply.github.com>
2020-10-16 19:53:19 -0300

    Create Venta.java

---
+++ Entidades/Venta.java
@@ -0,0 +1,16 @@
+package Entidades
+
+public class Venta{
+
+private Producto producto;
+private Precio precio;
+private Cantidad cantidad;
+
+public void Venta(){
+
+}
+
+public void registroVenta(){
+
+}
+}
