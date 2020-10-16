a82efbfc3d22c79df5809119b9bb2ddd4da4e475
Gianni González <giannigonzalez@hotmail.com>
2020-10-16 18:21:58 -0300

    Clase Proovedores

--- Enums/TipoCobertura.java
+++
@@ -1,5 +0,0 @@
-package Enums;
-
-public enum TipoCobertura {
-    TOTAL,CONTRA_TERCEROS,PERSONAL; 
-}
--- Enums/TipoVehiculo.java
+++
@@ -1,5 +0,0 @@
-package Enums;
-
-public enum TipoVehiculo {
-    AUTO,CAMION,CAMIONETA,MOTO;
-}
