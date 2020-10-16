4d48aff5364a8af4126932c4ba64b0cf555ace23
IvanBerenguer <36020676+IvanBerenguer@users.noreply.github.com>
2020-10-16 20:02:00 -0300

    Update Venta.java

--- Entidades/Venta.java
+++ Entidades/Venta.java
@@ -3,8 +3,29 @@ package Entidades
 public class Venta{
 
 private Producto producto;
-private Precio precio;
-private Cantidad cantidad;
+private int cantidad;
+private long precio;
+
+public void definir(int cantidad, long precio){
+this.cantidad = cantidad;
+this.precio = precio;
+}
+
+public int getCantidad() {
+        return cantidad;
+    }
+
+public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+public long getPrecio() {
+        return precio;
+    }
+
+public void setPrecio(long precio) {
+        this.precio = precio;
+    }
 
 public void Venta(){
 
