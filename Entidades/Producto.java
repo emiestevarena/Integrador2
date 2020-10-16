d4b346c0336a11223bb2353166c29e137d4f9ee9
paupaturzo <72951377+paupaturzo@users.noreply.github.com>
2020-10-16 19:43:05 -0300

    Add files via upload

---
+++ Entidades/Producto.java
@@ -0,0 +1,74 @@
+
+package trabajogrupo;
+
+
+public class Producto {
+    public int id_producto;
+    public String nombre;
+    public int precios;
+    public String proveedor;
+    public int cantidad;
+    
+    public Producto () {
+    }
+
+    public Producto(int id_producto, String nombre, int precios, String proveedor, int cantidad) {
+        this.id_producto = id_producto;
+        this.nombre = nombre;
+        this.precios = precios;
+        this.proveedor = proveedor;
+        this.cantidad = cantidad;
+    }
+
+    
+    
+    
+    public int getId_producto() {
+        return id_producto;
+    }
+
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecios() {
+        return precios;
+    }
+
+    public void setPrecios(int precios) {
+        this.precios = precios;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void Asignacion() {
+
+    }
+    public void Retorno() {
+        
+    }
+    
+}
+
